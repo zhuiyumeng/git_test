608e6ae5daf97aded6ee40a13ba2bc4af1788870
wyt <1697050950@qq.com>
2022-08-24 08:31:10 +0800

    admin提交

--- src/main/java/git/GitTest.java
+++ src/main/java/git/GitTest.java
@@ -2,9 +2,6 @@ package git;
 
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println(123);
-        System.out.println(123);
-        System.out.println(123);
-        System.out.println(123);
+        System.out.println("admin");
     }
 }
